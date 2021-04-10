2824162a1f7ebb97d77bd01aef360904cc8c8f7b
Dave <daveclydesdale@gmail.com>
2021-04-11 00:47:36 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react' 
 import './App.css';
+import queryString from 'query-string'
+import axios from 'axios'
+import spinner from 'Assets/Loaders/spin_white.svg'
 
-function App() {
+
+const App = ({}) => {
+
+  const [ state, setState ] = useState({serverData:null, redirecting:false})
+  const { serverData, redirecting } = state
+  const { access_token } = queryString.parse(window.location.search)
+
+  const callAPI = async (token) => {
+
+    const config = {
+      data : {
+        "scope": "user-top-read"
+      },
+      headers:{
+        'Authorization': 'Bearer ' + token
+      }
+    }
+    const {data} = await axios.get(`https://api.spotify.com/v1/me/top/tracks?time_range=long_term`, config)
+
+    const formattedObject = data.items.map(item => {
+      return {
+        name:item.name,
+        artist: {
+          name: item.artists[0].name,
+          href: item.artists[0].href,
+          uri: item.artists[0].uri
+        },
+        album:{
+          name: item.album.name,
+          href: item.album.href,
+          uri: item.album.uri,
+          images: item.album.images,
+          release_date: item.album.release_date,
+        },
+        preview_url:item.preview_url,
+        href:item.href
+      }
+    })
+
+    console.log(formattedObject)
+    setState({...state, serverData: formattedObject})
+  }
+
+  useEffect(()=>{
+    if(access_token){ callAPI(access_token) }
+  },[])
+
+  const handleLogin = () => {
+    window.location="http://localhost:8888/login";
+    setState({...state, redirecting:true})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div onClick={handleLogin} className="loginBtn">{!redirecting?"Login to Spotify":<img className={"spinner"} src={spinner} ></img>}</div>
     </div>
   );
 }
